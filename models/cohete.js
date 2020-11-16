663d6eb821d075cdca64b762f8aec7d7176b4f3f
Jose Maria Farfan <magoalakai@gmail.com>
2020-11-16 11:08:20 +0100

    initial Commit

---
+++ models/cohete.js
@@ -0,0 +1,12 @@
+var Car = /** @class */ (function () {
+    function Car(plate, brand, color) {
+        this.wheels = new Array();
+        this.plate = plate;
+        this.brand = brand;
+        this.color = color;
+    }
+    Car.prototype.addWheel = function (wheel) {
+        this.wheels.push(wheel);
+    };
+    return Car;
+}());
